56c53c6fc795660785660d6e0ba45592584875cc
Dhruv Sharma <techyguy10@outlook.com>
2024-10-05 15:07:40 +0400

    Update appTemp.py

--- appTemp.py
+++ appTemp.py
@@ -1,1 +1,31 @@
-pass
+import customtkinter as ctk
+import json
+
+
+app = ctk.CTk()
+app.geometry("600x350")
+app.resizable(False, False)
+app.title("Project Castor")
+
+
+with open('jsonedPlanets.json', 'r') as openfile:
+
+	# Reading from json file
+	json_object = json.load(openfile)
+
+for i in range(100):
+    print(json_object[i]["planetName"])
+
+
+def combobox_callback(choice):
+    print("combobox dropdown clicked:", choice)
+
+combobox = ctk.CTkComboBox(app, values=["option 1", "option 2"],
+                                     command=combobox_callback)
+combobox.set("option 2")
+
+combobox.place(x=10, y=10)
+
+
+
+app.mainloop()

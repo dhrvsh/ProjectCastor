866fe477af192c5bf61238102eb7e0d29970bd93
Dhruv Sharma <techyguy10@outlook.com>
2024-10-05 13:16:09 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import customtkinter as ctk
 from PIL import Image
 
-app = ctk.CTk()
-app.geometry("600x350")
-app.resizable(False, False)
-app.title("Project Castor")
+welcomeScreen = ctk.CTk()
+welcomeScreen.geometry("600x350")
+welcomeScreen.resizable(False, False)
+welcomeScreen.title("Project Castor")
 
 
 backdrop = ctk.CTkImage(light_image=Image.open("space.jpg"),
                                   dark_image=Image.open("space.jpg"),
                                   size=(660, 371))
 
-backdropSprite = ctk.CTkLabel(app, image=backdrop, text="")
+backdropSprite = ctk.CTkLabel(welcomeScreen, image=backdrop, text="")
 backdropSprite.place(x=0, y=0)
 
-title = ctk.
+title = ctk.CTkLabel(welcomeScreen, text="Welcome to Project Castor.", fg_color="transparent", font=("Bungee Tint", 10)).place(x=10, y=10)
 
-app.mainloop()
+welcomeScreen.mainloop()

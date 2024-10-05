81fc8be1c36be44ae99755fc5df8e702d09dce25
Dhruv Sharma <techyguy10@outlook.com>
2024-10-05 13:10:48 +0400

    added font, updated code with backdrop for welcome screen

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import customtkinter
+import customtkinter as ctk
+from PIL import Image
 
-app = customtkinter.CTk()
+app = ctk.CTk()
 app.geometry("600x350")
 app.resizable(False, False)
 app.title("Project Castor")
+
+
+backdrop = ctk.CTkImage(light_image=Image.open("space.jpg"),
+                                  dark_image=Image.open("space.jpg"),
+                                  size=(660, 371))
+
+backdropSprite = ctk.CTkLabel(app, image=backdrop, text="")
+backdropSprite.place(x=0, y=0)
+
+title = ctk.
 
 app.mainloop()

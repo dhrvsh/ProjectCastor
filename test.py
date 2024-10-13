bd9d03d2f114eba0143433c9e553e2fa7d25e0dc
Dhruv Sharma <techyguy10@outlook.com>
2024-10-13 13:41:07 +0400

    added joystick controls in test.py

--- test.py
+++ test.py
@@ -1,19 +1,73 @@
-import pyglet
+import pygame
 
-# get a list of all low-level input devices:
-devices = pyglet.input.get_devices()
+pygame.init()
 
-# get a list of all controllers:
-controllers = pyglet.input.get_controllers()
+joystick = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
 
-# get a list of all joysticks:
-joysticks = pyglet.input.get_joysticks()
+buttonPressed = ""
 
-# get a list of tablets:
-tablets = pyglet.input.get_tablets()
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONDOWN:
+            buttonID = event.button
+            buttons = {
+                0: "A",
+                1: "B",
+                2: "X",
+                3: "Y",
+                4: "-",
+                5: "Home",
+                6: "+",
+                7: "Left Joystick Pressed",
+                8: "Right Joystick Pressed",
+                9: "L",
+                10: "R",
+                11: "D-Pad Up",
+                12: "D-Pad Down",
+                13: "D-Pad Left",
+                14: "D-Pad Right",
+                15: "Capture",
+            }
 
-# get an Apple Remote, if available:
-remote = pyglet.input.get_apple_remote()
+            buttonPressed = buttons[buttonID]
 
-# create a ControllerManager instance:
-controller_manager = pyglet.input.ControllerManager()
+
+            if buttonPressed == "Home":
+                run = False
+
+        elif event.type == pygame.JOYAXISMOTION:
+            axis = event.axis
+            value = event.value
+
+            if axis == 0:
+                if value > 0.5:
+                    buttonPressed = "Left joystick moving right"
+                elif value < -0.5:
+                    buttonPressed = "Left joystick moving left"
+            elif axis == 1:
+                if value < -0.5:
+                    buttonPressed = "Left joystick moving up"
+                elif value > 0.5:
+                    buttonPressed = "Left joystick moving down"
+            
+
+            elif axis == 2:
+                if value > 0.5:
+                    buttonPressed = "Right joystick moving right"
+                elif value < -0.5:
+                    buttonPressed = "Right joystick moving left"
+            
+            elif axis == 3:  # Right joystick up-down axis
+                if value < -0.5:
+                    buttonPressed = "Right joystick moving up"
+                elif value > 0.5:
+                    buttonPressed = "Right joystick moving down"
+
+            elif axis == 4:
+                if value > 0.5:
+                    buttonPressed = "ZL"
+            elif axis == 5:
+                if value > 0.5:
+                    buttonPressed = "ZR"
+pygame.quit()

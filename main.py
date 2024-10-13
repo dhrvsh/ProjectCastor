f2bfbc68f922a661e864e76ac7d42bed34061e60
Dhruv Sharma <techyguy10@outlook.com>
2024-10-13 14:12:02 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import customtkinter as ctk
 from PIL import Image
 import json
+import pygame
+
+pygame.init()
+
+joystick = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+
+buttonPressed = ""
 
 welcomeScreen = ctk.CTk()
-welcomeScreen.geometry("600x350")
-welcomeScreen.resizable(False, False)
+welcomeScreen.geometry("1920x1080")
+# welcomeScreen.resizable(False, False)
 welcomeScreen.title("Project Castor")
 welcomeScreen.iconbitmap("icon.ico")
 
 backdrop = ctk.CTkImage(light_image=Image.open("space.png"),
                                   dark_image=Image.open("space.png"),
-                                  size=(600, 350))
+                                  size=(1920, 1080))
 
 backdropSprite = ctk.CTkLabel(welcomeScreen, image=backdrop, text="")
 backdropSprite.place(x=0, y=0)
